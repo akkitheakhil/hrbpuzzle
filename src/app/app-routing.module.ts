f88fca16660ea1bcf80c5ee4519d77006cf542bb
Akkitheakhil <akkitheakhil>
2019-06-12 22:59:30 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { QuizComponent } from './pages/quiz/quiz.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: QuizComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
